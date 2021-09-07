cd3c4b7e0c892a35e38b501c98f4a38fa0ab96f5
tiendungid1 <tiendungid1@gmail.com>
2021-09-07 16:40:22 +0700

    Example swagger

--- src/database/migrations/20201119231620_create_user_roles.js
+++ src/database/migrations/20201119231620_create_user_roles.js
@@ -5,8 +5,10 @@
 export function up(knex) {
     return knex.schema.createTable('users_roles', table => {
         table.increments('id');
-        table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
-        table.integer('role_id').unsigned().references('id').inTable('roles').onDelete('CASCADE');
+        table.integer('user_id').unsigned().references('id').inTable('users')
+            .onDelete('CASCADE');
+        table.integer('role_id').unsigned().references('id').inTable('roles')
+            .onDelete('CASCADE');
     });
 }
 
